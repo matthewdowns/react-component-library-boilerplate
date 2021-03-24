c1826dda1ecb20cb57cfd860ca7a787bc2f4f996
Matthew Downs <matthew.downsc@gmail.com>
2021-03-24 12:11:09 -0500

    Update prepublish script

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -2,37 +2,36 @@ const { exec } = require('child_process');
 const { exit } = require('process');
 const { version } = require('../package.json');
 
+isGitBranch('matthewdowns/setup-publishing')
+    .then(() => isGitDirectoryClean()
+        .catch(e => error(e)))
+    .catch(e => error(e));
+
 async function isGitBranch(branch) {
     return await new Promise((resolve, reject) => exec('git rev-parse --abbrev-ref HEAD', (err, stdout) => {
         let error = undefined;
         if (err) error = err.message;
-        else if (typeof stdout !== 'string') throw new Error('Unrecognized stdout result');
-        else if (stdout.trim() !== branch) throw new Error(`You need to be on branch "${branch}" to publish, current branch: "${stdout}"`);
+        else if (typeof stdout !== 'string') error = 'Unrecognized stdout result';
+        else if (stdout.trim() !== branch) error = `You need to be on branch "${branch}" to publish, current branch: "${stdout}"`;
 
         if (error) reject(error);
         else resolve();
     }));
 }
 
-async function isGitClean() {
+async function isGitDirectoryClean() {
     return await new Promise((resolve, reject) => exec('git status --porcelain', (err, stdout) => {
         let error = undefined;
         if (err) error = err.message;
-        console.log(stdout);
-        const isString = typeof stdout === 'string';
-        console.log(isString);
-        if (isString) {
-            const isEmpty = stdout.length === 0;
-            console.log(isEmpty);
-        }
+        else if (typeof stdout !== 'string') error = 'Unrecognized stdout result';
+        else if (stdout.trim().length !== 0) error = 'Git directory is not clean';
 
         if (error) reject(error);
         else resolve();
     }));
 }
 
-try {
-    isGitBranch('matthewdowns/setup-publishing').then(() => isGitClean());
-} catch(e) {
+function error(e) {
     console.error(e);
+    exit(1);
 }
