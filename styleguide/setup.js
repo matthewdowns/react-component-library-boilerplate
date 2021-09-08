d5732d5b6f79fa81d03e3dd9f752bd532051173c
Matthew Downs <matthew.downsc@gmail.com>
2021-09-08 17:14:10 -0500

    Updating styleguide

---
+++ styleguide/setup.js
@@ -0,0 +1,9 @@
+if (process.env.NODE_ENV !== 'production') {
+    const React = require('react');
+    const ReactDOM = require('react-dom');
+
+    const axe = require('@axe-core/react');
+    axe(React, ReactDOM, 1000, undefined, {
+        include: [['[data-preview]']]
+    });
+}
